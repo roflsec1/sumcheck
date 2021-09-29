957b5a8b42f191f5069851fc5a323200c7ccee80
wh1t3 <64658116+wh1t3-h4t@users.noreply.github.com>
2021-09-29 04:46:39 -0400

    Checksum ^_^

--- experiment5.py
+++ experiment5.py
@@ -1,28 +1,51 @@
-#Sender
-#10011001111000100010010010000100
-#100110011110001000100100
-#1001100111100010 
+#AUTHOR = wh1t3-h4t
+
+#10011001111000100010010010000100 (32-bit)
+#100110011110001000100100 (24-bit)
+#1001100111100010 (16-bit) 
 #0010010010000100
-#_Author_: wh1t3-h4t
+
+from time import *
+from os import *
+
+print(get_terminal_size())
+print("Recommended size: 80 x 24 or greater!")
+sleep(3)
+if uname().sysname == "Linux":
+	system('clear')
+
+print("""
+  ██████  █    ██  ███▄ ▄███▓  ▄████▄   ██░ ██  ▓█████  ▄████▄  ██ ▄█▀
+▒██    ▒  ██  ▓██▒▓██▒▀█▀ ██▒ ▒██▀ ▀█ ▒▓██░ ██  ▓█   ▀ ▒██▀ ▀█  ██▄█▒ 
+░ ▓██▄   ▓██  ▒██░▓██    ▓██░ ▒▓█    ▄░▒██▀▀██  ▒███   ▒▓█    ▄▓███▄░ 
+  ▒   ██▒▓▓█  ░██░▒██    ▒██ ▒▒▓▓▄ ▄██ ░▓█ ░██  ▒▓█  ▄▒▒▓▓▄ ▄██▓██ █▄ 
+▒██████▒▒▒▒█████▓ ▒██▒   ░██▒░▒ ▓███▀  ░▓█▒░██▓▒░▒████░▒ ▓███▀ ▒██▒ █▄
+▒ ▒▓▒ ▒ ░░▒▓▒ ▒ ▒ ░ ▒░   ░  ░░░ ░▒ ▒    ▒ ░░▒░▒░░░ ▒░ ░░ ░▒ ▒  ▒ ▒▒ ▓▒
+░ ░▒  ░ ░░░▒░ ░ ░ ░  ░      ░   ░  ▒    ▒ ░▒░ ░░ ░ ░     ░  ▒  ░ ░▒ ▒░
+░  ░  ░   ░░░ ░ ░ ░      ░    ░         ░  ░░ ░    ░   ░       ░ ░░ ░ 
+      ░     ░            ░    ░ ░       ░  ░  ░░   ░   ░ ░     ░  ░   
+""")
+sleep(1)
 
 m = 8
 data = input("Enter data whose size is a multiple of {}: ".format(m))
 
-while len(data)%m!=0 and len(data)<=m:
-	data = input("INVALID! Please enter data whose size is a multiple of 8: ")
+while len(data)%m!=0 and len(data)<=m: #sender
+	data = input("INVALID! Please enter data whose size is a multiple of 2: ")
 
 k = len(data)//m
 data_list = [data[i:i+m] for i in range(0, len(data), m)]
 bin_sum1 = 0b0
 for i in range(k):
 	bin_sum1 = bin_sum1 + int(data_list[i], 2)
-	print(bin(bin_sum1))
+	#print(bin(bin_sum1)) #for debugging
 	if len(bin(bin_sum1)) > m+2 and bin(bin_sum1)[2] == '1':
 		bin_sum1 = bin_sum1 + 0b1
 		bin_sum1 = bin(bin_sum1)[0:2] + bin(bin_sum1)[3:] #cutting msb
 		bin_sum1 = int(bin_sum1, 2)
 
-if len(bin(bin_sum1)) < len(bin(int(data_list[1], 2))):
+_sum = bin(bin_sum1)[2:]
+if len(bin(bin_sum1)) < len(bin(int(data_list[0], 2))):
 	_sum = "0" * (m+2-len(bin(bin_sum1))) + bin(bin_sum1)[2:]
 
 print("Sum: " + _sum)
